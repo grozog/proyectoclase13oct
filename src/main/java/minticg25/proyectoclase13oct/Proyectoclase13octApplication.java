47fe79cb42213a0dc66ae369e12813b70940acc8
ANDRES <andres-022@hotmail.com>
2021-10-14 14:21:06 -0500

    proyecto spring boot

---
+++ src/main/java/minticg25/proyectoclase13oct/Proyectoclase13octApplication.java
@@ -0,0 +1,13 @@
+package minticg25.proyectoclase13oct;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Proyectoclase13octApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Proyectoclase13octApplication.class, args);
+	}
+
+}
---
+++ src/test/java/minticg25/proyectoclase13oct/Proyectoclase13octApplicationTests.java
@@ -0,0 +1,13 @@
+package minticg25.proyectoclase13oct;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Proyectoclase13octApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
